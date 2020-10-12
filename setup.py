5c2e4c993461191e4eb1981a665cb6b6f8645d33
Monica Shapiro <monshapiro@gmail.com>
2020-10-12 10:57:59 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
-
 setup(
    name='plepy',
    version='1.0',
@@ -10,5 +7,13 @@ setup(
    author='Monica Shapiro',
    author_email='monshapiro@gmail.com',
    packages=['plepy'],  #same as name
-   install_requires=requirements, #external packages as dependencies
+   install_requires=[
+       'numpy ~= 1.19',
+       'pyomo ~= 5.7',
+       'scipy ~= 1.5',
+       'matplotlib ~= 3.3',
+       'seaborn ~= 0.10',
+       'pandas ~= 1.1'
+   ], #external packages as dependencies
+   tests_require=['pytest']
 )
